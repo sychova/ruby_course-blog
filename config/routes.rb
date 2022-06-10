fb6c9c4ebc9e1bf7c0f0f47b28e1e1e1d9b37ec2
Anastasiya Sychova <anastasiya.sychova@gmail.com>
2022-06-10 17:58:10 +0300

    crud ready

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  root 'pages#home'
-  get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index]
-  #try this at work:
-  #get 'show', to: 'articles#show'
-  #I mean it doesn't specifically point to the show html but still sees it, how does it know that the params is id?
-  #check what routes do you have and maybe run the "rails generate controller articles"
-  #or try rails generate scaffold Article title:string description:text
+  root "pages#home"
+  get "about", to: "pages#about"
+  resources :articles
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,5 +17,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_09_100520) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
